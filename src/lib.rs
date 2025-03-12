9d63c13e417b782e2c511548710b8b3c031959bc
bWFuanVzYWth <origin_0110@outlook.com>
2025-03-13 01:52:24 +0800

    新增cargo clippy设置

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+#![deny(
+    clippy::indexing_slicing,      // 禁止直接索引访问（可能导致越界 panic）
+    clippy::unwrap_used,           // 禁止使用 `unwrap`
+    clippy::expect_used,           // 禁止使用 `expect`
+    clippy::panic_in_result_fn,    // 禁止在返回 `Result` 的函数中调用 `panic!`
+    clippy::todo,                  // 禁止使用 `todo!`
+    clippy::unreachable,           // 禁止使用 `unreachable!`
+    clippy::unimplemented          // 禁止使用 `unimplemented!`
+)]
+#![warn(
+    clippy::pedantic,              // 启用所有严格的 Lint
+    clippy::cargo,                 // 启用与 Cargo 相关的 Lint
+    clippy::nursery                // 启用实验性的 Lint
+)]
+
 pub mod blueprint;
 pub mod error;
 pub mod io;
