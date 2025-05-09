03b12d5b637483720cf08d992d710e0fc133f505
linlin <52383012+bWFuanVzYWth@users.noreply.github.com>
2025-05-10 01:46:37 +0800

    新增一个测试代码覆盖率的备忘

--- src/lib.rs
+++ src/lib.rs
@@ -29,5 +29,6 @@ pub mod tesselation_structure;
 
 // TODO 给已经基本稳定下来的函数写文档
 // TODO cargo clippy --fix -- -D clippy::pedantic -D clippy::cargo -D clippy::nursery
+// TODO cargo tarpaulin --ignore-tests
 
 // TODO 消除concat
