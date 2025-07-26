a45a1dc2016450fcea6e69db33ed1750ee77fed8
linlin <52383012+bWFuanVzYWth@users.noreply.github.com>
2025-07-27 03:39:38 +0800

    重命名一些变量与rfc1321一致提高可读性

--- src/blueprint/codec/md5f/mod.rs
+++ src/blueprint/codec/md5f/mod.rs
@@ -1,4 +1,4 @@
-const K_MD5: [u32; 64] = [
+const T_MD5: [u32; 64] = [
     0xd76a_a478,
     0xe8c7_b756,
     0x2420_70db,
@@ -65,8 +65,8 @@ const K_MD5: [u32; 64] = [
     0xeb86_d391,
 ];
 
-const K_MD5F: [u32; 64] = {
-    let mut arr = K_MD5;
+const T_MD5F: [u32; 64] = {
+    let mut arr = T_MD5;
     arr[1] = 0xe8d7_b756;
     arr[6] = 0xa830_4623;
     arr[12] = 0x6b9f_1122;
@@ -78,8 +78,8 @@ const K_MD5F: [u32; 64] = {
     arr
 };
 
-const K_MD5FC: [u32; 64] = {
-    let mut arr = K_MD5;
+const T_MD5FC: [u32; 64] = {
+    let mut arr = T_MD5;
     arr[1] = 0xe8d7_b756;
     arr[3] = 0xc1bd_ceef;
     arr[6] = 0xa830_4623;
@@ -110,14 +110,14 @@ const S42: u32 = 10;
 const S43: u32 = 15;
 const S44: u32 = 21;
 
-const INIT_MD5: [u32; 4] = [
+const INIT_BUFFER_MD5: [u32; 4] = [
     u32::from_le_bytes([0x01, 0x23, 0x45, 0x67]),
     u32::from_le_bytes([0x89, 0xab, 0xcd, 0xef]),
     u32::from_le_bytes([0xfe, 0xdc, 0xba, 0x98]),
     u32::from_le_bytes([0x76, 0x54, 0x32, 0x10]),
 ];
 
-const INIT_MD5F: [u32; 4] = [
+const INIT_BUFFER_MD5F: [u32; 4] = [
     u32::from_le_bytes([0x01, 0x23, 0x45, 0x67]),
     u32::from_le_bytes([0x89, 0xab, 0xdc, 0xef]),
     u32::from_le_bytes([0xfe, 0xdc, 0xba, 0x98]),
@@ -133,7 +133,7 @@ pub enum Algo {
 
 pub struct MD5 {
     state: [u32; 4],
-    k_table: &'static [u32; 64],
+    table: &'static [u32; 64],
 }
 
 pub type MD5Hash = [u32; 4];
@@ -192,19 +192,20 @@ impl MD5 {
     }
 
     const fn new(algo: Algo) -> Self {
-        let (s, k_table) = match algo {
-            Algo::MD5 => (INIT_MD5, &K_MD5),
-            Algo::MD5F => (INIT_MD5F, &K_MD5F),
-            Algo::MD5FC => (INIT_MD5F, &K_MD5FC), // 注意此处用的仍然是INIT_MD5F
+        let (state, table) = match algo {
+            Algo::MD5 => (INIT_BUFFER_MD5, &T_MD5),
+            Algo::MD5F => (INIT_BUFFER_MD5F, &T_MD5F),
+            Algo::MD5FC => (INIT_BUFFER_MD5F, &T_MD5FC), // 注意此处用的仍然是INIT_MD5F
         };
 
-        Self { state: s, k_table }
+        Self { state, table }
     }
 
     #[expect(clippy::many_single_char_names)]
-    fn update_block(&mut self, buf: &[u8; 64]) {
+    fn update_block(&mut self, block: &[u8; 64]) {
         let x: [u32; 16] = std::array::from_fn(|i| {
-            u32::from_le_bytes([buf[i * 4], buf[i * 4 + 1], buf[i * 4 + 2], buf[i * 4 + 3]])
+            let i4 = i * 4;
+            u32::from_le_bytes([block[i4], block[i4 + 1], block[i4 + 2], block[i4 + 3]])
         });
 
         let mut a = self.state[0];
@@ -212,77 +213,77 @@ impl MD5 {
         let mut c = self.state[2];
         let mut d = self.state[3];
 
-        /* Round 1 */
-        a = Self::ff(a, b, c, d, x[0], S11, self.k_table[0]);
-        d = Self::ff(d, a, b, c, x[1], S12, self.k_table[1]);
-        c = Self::ff(c, d, a, b, x[2], S13, self.k_table[2]);
-        b = Self::ff(b, c, d, a, x[3], S14, self.k_table[3]);
-        a = Self::ff(a, b, c, d, x[4], S11, self.k_table[4]);
-        d = Self::ff(d, a, b, c, x[5], S12, self.k_table[5]);
-        c = Self::ff(c, d, a, b, x[6], S13, self.k_table[6]);
-        b = Self::ff(b, c, d, a, x[7], S14, self.k_table[7]);
-        a = Self::ff(a, b, c, d, x[8], S11, self.k_table[8]);
-        d = Self::ff(d, a, b, c, x[9], S12, self.k_table[9]);
-        c = Self::ff(c, d, a, b, x[10], S13, self.k_table[10]);
-        b = Self::ff(b, c, d, a, x[11], S14, self.k_table[11]);
-        a = Self::ff(a, b, c, d, x[12], S11, self.k_table[12]);
-        d = Self::ff(d, a, b, c, x[13], S12, self.k_table[13]);
-        c = Self::ff(c, d, a, b, x[14], S13, self.k_table[14]);
-        b = Self::ff(b, c, d, a, x[15], S14, self.k_table[15]);
+        // Round 1
+        a = Self::ff(a, b, c, d, x[0], S11, self.table[0]);
+        d = Self::ff(d, a, b, c, x[1], S12, self.table[1]);
+        c = Self::ff(c, d, a, b, x[2], S13, self.table[2]);
+        b = Self::ff(b, c, d, a, x[3], S14, self.table[3]);
+        a = Self::ff(a, b, c, d, x[4], S11, self.table[4]);
+        d = Self::ff(d, a, b, c, x[5], S12, self.table[5]);
+        c = Self::ff(c, d, a, b, x[6], S13, self.table[6]);
+        b = Self::ff(b, c, d, a, x[7], S14, self.table[7]);
+        a = Self::ff(a, b, c, d, x[8], S11, self.table[8]);
+        d = Self::ff(d, a, b, c, x[9], S12, self.table[9]);
+        c = Self::ff(c, d, a, b, x[10], S13, self.table[10]);
+        b = Self::ff(b, c, d, a, x[11], S14, self.table[11]);
+        a = Self::ff(a, b, c, d, x[12], S11, self.table[12]);
+        d = Self::ff(d, a, b, c, x[13], S12, self.table[13]);
+        c = Self::ff(c, d, a, b, x[14], S13, self.table[14]);
+        b = Self::ff(b, c, d, a, x[15], S14, self.table[15]);
 
-        /* Round 2 */
-        a = Self::gg(a, b, c, d, x[1], S21, self.k_table[16]);
-        d = Self::gg(d, a, b, c, x[6], S22, self.k_table[17]);
-        c = Self::gg(c, d, a, b, x[11], S23, self.k_table[18]);
-        b = Self::gg(b, c, d, a, x[0], S24, self.k_table[19]);
-        a = Self::gg(a, b, c, d, x[5], S21, self.k_table[20]);
-        d = Self::gg(d, a, b, c, x[10], S22, self.k_table[21]);
-        c = Self::gg(c, d, a, b, x[15], S23, self.k_table[22]);
-        b = Self::gg(b, c, d, a, x[4], S24, self.k_table[23]);
-        a = Self::gg(a, b, c, d, x[9], S21, self.k_table[24]);
-        d = Self::gg(d, a, b, c, x[14], S22, self.k_table[25]);
-        c = Self::gg(c, d, a, b, x[3], S23, self.k_table[26]);
-        b = Self::gg(b, c, d, a, x[8], S24, self.k_table[27]);
-        a = Self::gg(a, b, c, d, x[13], S21, self.k_table[28]);
-        d = Self::gg(d, a, b, c, x[2], S22, self.k_table[29]);
-        c = Self::gg(c, d, a, b, x[7], S23, self.k_table[30]);
-        b = Self::gg(b, c, d, a, x[12], S24, self.k_table[31]);
+        // Round 2
+        a = Self::gg(a, b, c, d, x[1], S21, self.table[16]);
+        d = Self::gg(d, a, b, c, x[6], S22, self.table[17]);
+        c = Self::gg(c, d, a, b, x[11], S23, self.table[18]);
+        b = Self::gg(b, c, d, a, x[0], S24, self.table[19]);
+        a = Self::gg(a, b, c, d, x[5], S21, self.table[20]);
+        d = Self::gg(d, a, b, c, x[10], S22, self.table[21]);
+        c = Self::gg(c, d, a, b, x[15], S23, self.table[22]);
+        b = Self::gg(b, c, d, a, x[4], S24, self.table[23]);
+        a = Self::gg(a, b, c, d, x[9], S21, self.table[24]);
+        d = Self::gg(d, a, b, c, x[14], S22, self.table[25]);
+        c = Self::gg(c, d, a, b, x[3], S23, self.table[26]);
+        b = Self::gg(b, c, d, a, x[8], S24, self.table[27]);
+        a = Self::gg(a, b, c, d, x[13], S21, self.table[28]);
+        d = Self::gg(d, a, b, c, x[2], S22, self.table[29]);
+        c = Self::gg(c, d, a, b, x[7], S23, self.table[30]);
+        b = Self::gg(b, c, d, a, x[12], S24, self.table[31]);
 
-        /* Round 3 */
-        a = Self::hh(a, b, c, d, x[5], S31, self.k_table[32]);
-        d = Self::hh(d, a, b, c, x[8], S32, self.k_table[33]);
-        c = Self::hh(c, d, a, b, x[11], S33, self.k_table[34]);
-        b = Self::hh(b, c, d, a, x[14], S34, self.k_table[35]);
-        a = Self::hh(a, b, c, d, x[1], S31, self.k_table[36]);
-        d = Self::hh(d, a, b, c, x[4], S32, self.k_table[37]);
-        c = Self::hh(c, d, a, b, x[7], S33, self.k_table[38]);
-        b = Self::hh(b, c, d, a, x[10], S34, self.k_table[39]);
-        a = Self::hh(a, b, c, d, x[13], S31, self.k_table[40]);
-        d = Self::hh(d, a, b, c, x[0], S32, self.k_table[41]);
-        c = Self::hh(c, d, a, b, x[3], S33, self.k_table[42]);
-        b = Self::hh(b, c, d, a, x[6], S34, self.k_table[43]);
-        a = Self::hh(a, b, c, d, x[9], S31, self.k_table[44]);
-        d = Self::hh(d, a, b, c, x[12], S32, self.k_table[45]);
-        c = Self::hh(c, d, a, b, x[15], S33, self.k_table[46]);
-        b = Self::hh(b, c, d, a, x[2], S34, self.k_table[47]);
+        // Round 3
+        a = Self::hh(a, b, c, d, x[5], S31, self.table[32]);
+        d = Self::hh(d, a, b, c, x[8], S32, self.table[33]);
+        c = Self::hh(c, d, a, b, x[11], S33, self.table[34]);
+        b = Self::hh(b, c, d, a, x[14], S34, self.table[35]);
+        a = Self::hh(a, b, c, d, x[1], S31, self.table[36]);
+        d = Self::hh(d, a, b, c, x[4], S32, self.table[37]);
+        c = Self::hh(c, d, a, b, x[7], S33, self.table[38]);
+        b = Self::hh(b, c, d, a, x[10], S34, self.table[39]);
+        a = Self::hh(a, b, c, d, x[13], S31, self.table[40]);
+        d = Self::hh(d, a, b, c, x[0], S32, self.table[41]);
+        c = Self::hh(c, d, a, b, x[3], S33, self.table[42]);
+        b = Self::hh(b, c, d, a, x[6], S34, self.table[43]);
+        a = Self::hh(a, b, c, d, x[9], S31, self.table[44]);
+        d = Self::hh(d, a, b, c, x[12], S32, self.table[45]);
+        c = Self::hh(c, d, a, b, x[15], S33, self.table[46]);
+        b = Self::hh(b, c, d, a, x[2], S34, self.table[47]);
 
-        /* Round 4 */
-        a = Self::ii(a, b, c, d, x[0], S41, self.k_table[48]);
-        d = Self::ii(d, a, b, c, x[7], S42, self.k_table[49]);
-        c = Self::ii(c, d, a, b, x[14], S43, self.k_table[50]);
-        b = Self::ii(b, c, d, a, x[5], S44, self.k_table[51]);
-        a = Self::ii(a, b, c, d, x[12], S41, self.k_table[52]);
-        d = Self::ii(d, a, b, c, x[3], S42, self.k_table[53]);
-        c = Self::ii(c, d, a, b, x[10], S43, self.k_table[54]);
-        b = Self::ii(b, c, d, a, x[1], S44, self.k_table[55]);
-        a = Self::ii(a, b, c, d, x[8], S41, self.k_table[56]);
-        d = Self::ii(d, a, b, c, x[15], S42, self.k_table[57]);
-        c = Self::ii(c, d, a, b, x[6], S43, self.k_table[58]);
-        b = Self::ii(b, c, d, a, x[13], S44, self.k_table[59]);
-        a = Self::ii(a, b, c, d, x[4], S41, self.k_table[60]);
-        d = Self::ii(d, a, b, c, x[11], S42, self.k_table[61]);
-        c = Self::ii(c, d, a, b, x[2], S43, self.k_table[62]);
-        b = Self::ii(b, c, d, a, x[9], S44, self.k_table[63]);
+        // Round 4
+        a = Self::ii(a, b, c, d, x[0], S41, self.table[48]);
+        d = Self::ii(d, a, b, c, x[7], S42, self.table[49]);
+        c = Self::ii(c, d, a, b, x[14], S43, self.table[50]);
+        b = Self::ii(b, c, d, a, x[5], S44, self.table[51]);
+        a = Self::ii(a, b, c, d, x[12], S41, self.table[52]);
+        d = Self::ii(d, a, b, c, x[3], S42, self.table[53]);
+        c = Self::ii(c, d, a, b, x[10], S43, self.table[54]);
+        b = Self::ii(b, c, d, a, x[1], S44, self.table[55]);
+        a = Self::ii(a, b, c, d, x[8], S41, self.table[56]);
+        d = Self::ii(d, a, b, c, x[15], S42, self.table[57]);
+        c = Self::ii(c, d, a, b, x[6], S43, self.table[58]);
+        b = Self::ii(b, c, d, a, x[13], S44, self.table[59]);
+        a = Self::ii(a, b, c, d, x[4], S41, self.table[60]);
+        d = Self::ii(d, a, b, c, x[11], S42, self.table[61]);
+        c = Self::ii(c, d, a, b, x[2], S43, self.table[62]);
+        b = Self::ii(b, c, d, a, x[9], S44, self.table[63]);
 
         self.state[0] = self.state[0].wrapping_add(a);
         self.state[1] = self.state[1].wrapping_add(b);
@@ -290,22 +291,21 @@ impl MD5 {
         self.state[3] = self.state[3].wrapping_add(d);
     }
 
-    fn process(&mut self, data: &[u8]) -> MD5Hash {
-        let chunks = data.array_chunks::<64>();
+    fn process(&mut self, input: &[u8]) -> MD5Hash {
+        let chunks = input.array_chunks::<64>();
         let remainder = chunks.remainder();
         let remainder_len = remainder.len();
-
-        let bit_len = (data.len() * 8).to_le_bytes();
-        let bit_len_offset = remainder_len + 1 + 55_usize.wrapping_sub(remainder_len) % 64;
+        let input_bit = (input.len() * 8).to_le_bytes();
+        let input_bit_offset = remainder_len + 1 + 55_usize.wrapping_sub(remainder_len) % 64;
 
         let last: [u8; 128] = std::array::from_fn(|idx| match idx {
             i if i < remainder_len => remainder[i],
             i if i == remainder_len => 0x80,
-            i if i >= bit_len_offset && i < bit_len_offset + 8 => bit_len[i - bit_len_offset],
+            i if i >= input_bit_offset && i < input_bit_offset + 8 => input_bit[i - input_bit_offset],
             _ /* i if i > remainder_len && i < bit_len_offset */ => 0x00,
         });
 
-        for chunk in chunks.chain(last[..bit_len_offset + 8].array_chunks::<64>()) {
+        for chunk in chunks.chain(last[..input_bit_offset + 8].array_chunks::<64>()) {
             self.update_block(chunk);
         }
 
