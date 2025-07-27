08f5a06d1592e7e701f3ed086008ff5c09a4dc72
linlin <52383012+bWFuanVzYWth@users.noreply.github.com>
2025-07-27 23:35:10 +0800

    新增球面区域的面积计算公式

--- src/dspbptk_blueprint/generator/tesselation/mod.rs
+++ src/dspbptk_blueprint/generator/tesselation/mod.rs
@@ -4,10 +4,12 @@ use crate::planet::unit_conversion::arc_from_grid;
 use arrayvec::ArrayVec;
 use std::f64::consts::FRAC_PI_2;
 
-const MODULE_TYPE_COUNT: usize = 6;
+// TODO 考虑内存布局，在不牺牲性能的前提下尽量优化下面的几个值，如果有必要换成常规vec
+/// 最多允许多少行建筑
+const MAX_ROW_COUNT: usize = 64;
 
-// TODO 密铺排列计算
-// TODO 重构，为不同的模块impl对应的方法
+/// 最多允许计算多少种基础模块
+const MAX_MODULE_TYPE_COUNT: usize = 6;
 
 #[derive(Debug)]
 pub struct Module {
@@ -37,8 +39,9 @@ impl Module {
     }
 
     // TODO 补文档，越详细越好，包括背后的数学原理
-    // 根据下一行模块尺寸计算中心y，使得模块的最低点高于edge_y
     // 不完整，用法参考photon.rs
+    /// 根据下一行模块尺寸计算中心y，使得模块的最低点高于`edge_y`\
+    /// 下一行可能已经超出了纬度限制，不一定存在
     #[must_use]
     pub fn calculate_next_y(&self, edge_y: f64) -> Option<f64> {
         let z_max_of_this_row = edge_y.sin();
@@ -54,16 +57,51 @@ impl Module {
     }
 }
 
-/// 这个函数不检查y是否超标
+/// 代表了一行的建筑数据，
+struct Row {
+    /// 这一行的建筑类型，也就是建筑在输入数组中对应的下标
+    module_type: usize,
+
+    /// 这一行模块的数量，注意是浮点数。不过浮点数可以精确的表示整数所以不用担心误差
+    count: f64,
+
+    /// 这一行模块的几何中心的高度，单位是弧度
+    center_y: f64,
+}
+
+/// 代表了一个缓存了重要数据的中间布局
+struct Draft {
+    rows: ArrayVec<Row, MAX_ROW_COUNT>,
+    each_type_count: ArrayVec<f64, MAX_MODULE_TYPE_COUNT>,
+    score: Option<f64>,
+}
+
+impl Draft {
+    // TODO 推入一排建筑，包括计算新行的y，数量之类的，更新评分之类的
+    pub fn push(self, module_type: usize) -> Self {
+        todo!()
+    }
+}
+
+/// 这个函数不检查y是否超标，超标解应该放在流程控制中排除
 /// 找出最缺的建筑，将其相对需求的倍率作为分数
 pub fn score(
-    each_type_module: &ArrayVec<f64, MODULE_TYPE_COUNT>,
-    need: &ArrayVec<f64, MODULE_TYPE_COUNT>,
+    each_type_count: &ArrayVec<f64, MAX_MODULE_TYPE_COUNT>,
+    need: &ArrayVec<f64, MAX_MODULE_TYPE_COUNT>,
 ) -> Option<f64> {
     // 除零将会产生`positive quiet NaN`，大于所有的数字
-    each_type_module
+    each_type_count
         .iter()
         .zip(need.iter())
         .map(|(module, need)| module / need)
         .min_by(f64::total_cmp)
+}
+
+// TODO 基于排列生成具体的蓝图，这个函数必须抽象出去，不然难以保证基于相同的基因生成的蓝图结果一致
+// 而且好像还需要考虑渐进式生成，根据已有排列和缓存信息生成下一列
+
+// 输入每种模块的需求比例，纬度限制，输出排列
+#[must_use]
+pub fn tesselation(modules: &[Module], need: &[f64]) -> Vec<usize> {
+    todo!()
 }
--- src/planet/mod.rs
+++ src/planet/mod.rs
@@ -1,1 +1,2 @@
 pub mod unit_conversion;
+pub mod area;
